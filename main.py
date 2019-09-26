f905df85736d1be6d81af1294b855c5141b6fa4b
Yannick Carissan <yannick.carissan@univ-amu.fr>
2019-09-26 14:13:45 +0200

    minor correction

--- main.py
+++ main.py
@@ -50,8 +50,7 @@ def main():
     for l in open(geomfile,"r").readlines()[1:]:
         fout.write(l)
     for q in list_q:
-        fout.write("{:s} {} {}
-                   {}\n".format("q",list_q[0],list_q[1],list_q[2]))
+        fout.write("{:s} {} {} {}\n".format("q",list_q[0],list_q[1],list_q[2]))
     fout.close()
 
 
