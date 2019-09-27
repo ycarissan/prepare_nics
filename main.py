6e6ca8326f4d769a957c2d35a52037d26f43777b
Yannick Carissan <yannick.carissan@univ-amu.fr>
2019-09-27 10:00:32 +0200

    add comment

--- main.py
+++ main.py
@@ -7,34 +7,51 @@ import xml.etree.ElementTree as ET
 import networkx as nx
 
 def generate_cml(geomfile):
+    """ Genere le fichier de descritpion de la geometrie en cml"""
     proc = subprocess.Popen(["babel","-ixyz",geomfile,"-ocml","geom.cml"],
                                universal_newlines=True)
     stdout, stderr = proc.communicate()
     return
 
 def main():
+    #
+    # Lecture des arguments 
+    #
     parser = argparse.ArgumentParser()
     parser.add_argument("xyzfile", help="geom file in the xyz format")
     args = parser.parse_args()
     geomfile = args.xyzfile
     print("Treating ",geomfile)
+    #
     # Detection des cycles
+    #
+    #  Qualified domain name
     qdn = "{http://www.xml-cml.org/schema}"
     generate_cml(geomfile)
+    #  Lecture du cml
     tree = ET.parse('geom.cml')
     root = tree.getroot()
     atomArray_el = root.find(qdn+'atomArray')
     bondArray_el = root.find(qdn+'bondArray')
+    #  Nombre d'atomes
     nat = len(atomArray_el)
+    #  Creation du graphe
     G = nx.Graph()
     G.add_nodes_from([i+1 for i in range(nat)])
+    #  On parcourt les liaisons et on cree le graphe
     for bond in bondArray_el:
         at1, at2 = str.split(bond.get('atomRefs2'))
         G.add_edge(at1,at2)
+    #  Dectection des cycles
     cycles = nx.minimum_cycle_basis(G)
     print(cycles)
+    #
     # Traitement des cycles
+    #
     list_q=[]
+    #
+    # Pour chaque cycle detecte on fait la moyenne des coordonnées de atomes
+    #
     for cycle in cycles:
         x=y=z=0
         for iat in cycle:
@@ -45,7 +62,9 @@ def main():
             z = z + float(atom.get("z3"))
         list_q.append([x/len(cycle),y/len(cycle),z/len(cycle)])
         print("q",x/len(cycle),y/len(cycle),z/len(cycle))
-#write final file
+    #
+    # Ecriture finale
+    #
     fout=open("molecule_q.xyz","w+")
     fout.write(str(len(list_q)+nat)+"\n")
     for l in open(geomfile,"r").readlines()[1:]:
