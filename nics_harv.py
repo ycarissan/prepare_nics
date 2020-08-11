c82487240d58ed80d260cdc9d309732e99d9c587
Yannick Carissan <ycarissan@gmail.com>
2020-08-11 15:34:38 +0200

    add grid generateion and cdist usage

--- nics_harv.py
+++ nics_harv.py
@@ -2,10 +2,34 @@
 
 import sys
 import numpy as np
+from scipy.spatial.distance import cdist
 import getopt
 import re
 import os
 import glob
+
+def closest_node(node, nodes):
+    #from https://codereview.stackexchange.com/questions/28207/finding-the-closest-point-to-a-list-of-points
+    return nodes[cdist([node], nodes).argmin()]
+
+def generate_cube(geom, nics_grid):
+    xmin = min(a['x'] for a in nics_grid)
+    xmax = max(a['x'] for a in nics_grid)
+    ymin = min(a['y'] for a in nics_grid)
+    ymax = max(a['y'] for a in nics_grid)
+    zmin = min(a['z'] for a in nics_grid)
+    zmax = max(a['z'] for a in nics_grid)
+    print(xmin, xmax, ymin, ymax, zmin, zmax)
+    npts = 100
+    tmp_lst = []
+    #Generate grid : there is probably more efficient
+    for x in np.linspace(xmin, xmax, npts):
+        for y in np.linspace(ymin, ymax, npts):
+            for z in np.linspace(zmin, zmax, npts):
+                tmp_lst.append([x,y,z])
+    grid = np.array(tmp_lst)
+    a = closest_node([1,1,1], grid)
+    print(a)
 
 def readlogfile(logfile):
     """Read a guassian output file and store the geometry and the nics values (if any)"""
@@ -51,7 +75,7 @@ def store_data(geom, nics_grid):
         fio.write("{0[label]:s} {0[x]:16.10f}  {0[y]:16.10f}  {0[z]:16.10f}\n".format(el))
     fio.close()
     nics_file = "nics.dat"
-    fio = open(nics_file,"a+")
+    fio = open(nics_file,"w")
     for el in nics_grid:
         fio.write("{0[x]:16.10f}  {0[y]:16.10f}  {0[z]:16.10f}  {0[nics]:16.10f}\n".format(el))
     fio.close()
@@ -93,10 +117,18 @@ def main():
 #  and the data for all these files
 #
   logfiles = sorted(glob.glob(dirname+"/"+radical+"_[0-9]*.log"))
+  geom=[]
+  nics_grid=[]
   for f in logfiles:
-     print("Treating "+f)
-     geom, nics_grid = readlogfile(f)
-     store_data(geom, nics_grid)
+     print("Extracting from {0:s} ".format(f), end='')
+     geom_tmp, nics_grid_tmp = readlogfile(f)
+     if len(geom)==0:
+         geom = geom_tmp
+         print("geometry and ".format(), end='')
+     nics_grid.extend(nics_grid_tmp)
+     print("NICS values")
+  store_data(geom, nics_grid)
+  generate_cube(geom, nics_grid)
 
 if __name__ == "__main__":
   main()
