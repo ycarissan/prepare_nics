46a59bb819e6e356bf05dd08322bb4e273f0abbd
Yannick Carissan <ycarissan@gmail.com>
2020-08-12 16:26:53 +0200

    generate a cube file

--- nics_harv.py
+++ nics_harv.py
@@ -8,11 +8,47 @@ import re
 import os
 import glob
 
+Bohr2Angstrom = 0.5291772108
+
+def generate_cubefile(geom, grid, grid_values, dx, dy, dz, nptx, npty, nptz):
+   fio = open('nics.cube', "w+")
+   nat = len(geom)
+   fio.write("head 1\n".format())
+   fio.write("head 2\n".format())
+   fio.write("{0:5d} {1[0]:12.6f} {1[1]:12.6f} {1[2]:12.6f}\n".format(nat,grid[0]/Bohr2Angstrom))
+   fio.write("{:5d} {:12.6f} {:12.6f} {:12.6f}\n".format(nptx, dx/Bohr2Angstrom,0,0))
+   fio.write("{:5d} {:12.6f} {:12.6f} {:12.6f}\n".format(npty, 0,dy/Bohr2Angstrom,0))
+   fio.write("{:5d} {:12.6f} {:12.6f} {:12.6f}\n".format(nptz, 0,0,dz/Bohr2Angstrom))
+   for el in geom:
+       if (el['label'].strip().lower() == "c"):
+           nuclear_charge = 6
+       elif (el['label'].strip().lower() == "h"):
+           nuclear_charge = 1
+       charge = 0.0
+       fio.write("{:5d} {:12.6f} {:12.6f} {:12.6f}  {:12.6f}\n".format(nuclear_charge, charge,
+           el['x']/Bohr2Angstrom, 
+           el['y']/Bohr2Angstrom, 
+           el['z']/Bohr2Angstrom))
+   idx = 0
+   for x in range(0,nptx+1):
+       for y in range(0,npty+1):
+           for z in range(0,nptz+1):
+               k=str(idx)
+               val = 0
+               if (k in grid_values.keys()):
+                   val = grid_values[k]
+               fio.write("{:12.6f}".format(val))
+               if (z % 6 == 5):
+                   fio.write("\n".format())
+               idx = idx + 1
+       fio.write("\n".format())
+
+
 def closest_node(node, nodes):
     #from https://codereview.stackexchange.com/questions/28207/finding-the-closest-point-to-a-list-of-points
     return nodes[cdist([node], nodes).argmin()]
 
-def generate_cube(geom, nics_grid):
+def generate_values_on_grid(geom, nics_grid):
     xmin = min(a['x'] for a in nics_grid)
     xmax = max(a['x'] for a in nics_grid)
     ymin = min(a['y'] for a in nics_grid)
@@ -21,16 +57,29 @@ def generate_cube(geom, nics_grid):
     zmax = max(a['z'] for a in nics_grid)
     print(xmin, xmax, ymin, ymax, zmin, zmax)
     npts = 50
+    nptx = npty = nptz = npts
     tmp_lst = []
     #Generate grid : there is probably more efficient
     print("Generating grid")
-    for x in np.linspace(xmin, xmax, npts):
-        for y in np.linspace(ymin, ymax, npts):
-            for z in np.linspace(zmin, zmax, npts):
+    for x in np.linspace(xmin, xmax, npts):         # see dz
+        for y in np.linspace(ymin, ymax, npts):     # see dz
+            for z in np.linspace(zmin, zmax, npts): # dz = ( zmax - zmin ) / ( npts - 1 )
                 tmp_lst.append([x,y,z])
     grid = np.array(tmp_lst)
-    a = closest_node([1,1,1], grid)
-    print(a)
+    grid_values = {}
+    dx = (xmax - xmin) / ( nptx - 1 ) # remember dz = ( zmax - zmin ) / ( npts - 1 )
+    dy = (ymax - ymin) / ( npty - 1 ) # idem
+    dz = (zmax - zmin) / ( nptz - 1 ) # idem
+    for el in nics_grid:
+        el_prox = closest_node([ el['x'], el['y'], el['z']], grid)
+        idz = ( el_prox[2] - zmin ) / dz
+        idy = ( el_prox[1] - ymin ) / dy
+        idx = ( el_prox[0] - xmin ) / dx
+        index = idx * npty * nptz + idy * nptz + idz
+        grid_values[str(int(index))] = el['nics']
+        #store the value as a pair index (in str format), nics value: it is much better than storing a large number of
+        # zero nics values on the grid
+    return grid, grid_values, dx, dy, dz, nptx, npty, nptz
 
 def readlogfile(logfile):
     """Read a guassian output file and store the geometry and the nics values (if any)"""
@@ -129,7 +178,8 @@ def main():
      nics_grid.extend(nics_grid_tmp)
      print("NICS values")
   store_data(geom, nics_grid)
-  generate_cube(geom, nics_grid)
+  grid, grid_values, dx, dy, dz, nptx, npty, nptz = generate_values_on_grid(geom, nics_grid)
+  generate_cubefile(geom, grid, grid_values, dx, dy, dz, nptx, npty, nptz)
 
 if __name__ == "__main__":
   main()
