ca836b631630f2a3d74da8f828ee3589844a685f
Yannick Carissan <ycarissan@gmail.com>
2020-08-13 13:20:03 +0200

    add tests

---
+++ test.py
@@ -0,0 +1,10 @@
+class RandomTest(unittest.TestCase):
+
+    """Test case utilisé pour tester les fonctions du module 'random'."""
+
+    def test_choice(self):
+        """Test le fonctionnement de la fonction 'random.choice'."""
+        liste = list(range(10))
+        elt = random.choice(liste)
+        # Vérifie que 'elt' est dans 'liste'
+        self.assertIn(elt, liste)
