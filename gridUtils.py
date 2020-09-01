630a98a1c498d247bc0e07aacb333cde7d2fe429
Yannick Carissan <ycarissan@gmail.com>
2020-09-01 10:24:56 +0200

    add 3D get idx

--- gridUtils.py
+++ gridUtils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import logging
 
 def getIdx(value, val_min, val_max=None, npts=None, dval=None):
     """
@@ -12,3 +11,25 @@ def getIdx(value, val_min, val_max=None, npts=None, dval=None):
     if (dval==None):
         dval = (val_max-val_min)/(npts - 1)
     return np.round((value-val_min)/dval)
+
+def getIdx_cube(x, y, z, xmin, xmax, nptx, ymin, ymax, npty, zmin, zmax, nptz):
+    """
+    Returns the index of a point on a 3D rectangular gridr
+    The cube order is assumed (from http://paulbourke.net/dataformats/cube/)
+       for (ix=0;ix<NX;ix++) {
+           for (iy=0;iy<NY;iy++) {
+               for (iz=0;iz<NZ;iz++) {
+                   printf("%g ",data[ix][iy][iz]);
+                   if (iz % 6 == 5)
+                       printf("\n");
+                   }
+               printf("\n");
+           }
+       }
+    """
+    idx = getIdx(x, xmin, xmax, nptx)
+    idy = getIdx(y, ymin, ymax, npty)
+    idz = getIdx(z, zmin, zmax, nptz)
+    index = idx * npty * nptz + idy * nptz + idz
+    return index
+
