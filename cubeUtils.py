9a68f8851530e5f6a8272300ad53a5a66f219183
Yannick Carissan <ycarissan@gmail.com>
2020-09-01 10:25:12 +0200

    merge utils

--- cubeUtils.py
+++ cubeUtils.py
@@ -1,5 +1,9 @@
 from constants import Bohr2Angstrom
 
+def generate_cubefile(geom, grid_values, dx, dy, dz, nptx, npty, nptz):
+    grid = None
+    return generate_cubefile(geom, grid, grid_values, dx, dy, dz, nptx, npty, nptz)
+
 def generate_cubefile(geom, grid, grid_values, dx, dy, dz, nptx, npty, nptz):
     """
     Generate a cube file for the given geometry and grid
@@ -9,8 +13,8 @@ def generate_cubefile(geom, grid, grid_values, dx, dy, dz, nptx, npty, nptz):
     nat = len(geom)
     fio.write("head 1\n".format())
     fio.write("head 2\n".format())
-    fio.write("{0:5d} {1[0]:12.6f} {1[1]:12.6f} {1[2]:12.6f}\n".format(
-        nat, grid[0] / Bohr2Angstrom))
+#    fio.write("{0:5d} {1[0]:12.6f} {1[1]:12.6f} {1[2]:12.6f}\n".format(
+#        nat, grid[0] / Bohr2Angstrom))
     fio.write("{:5d} {:12.6f} {:12.6f} {:12.6f}\n".format(
         nptx, dx / Bohr2Angstrom, 0, 0))
     fio.write("{:5d} {:12.6f} {:12.6f} {:12.6f}\n".format(
@@ -105,4 +109,39 @@ def generate_values_on_grid(geom, nics_grid, npts):
         # zero nics values on the grid
     return grid, grid_values, dx, dy, dz, nptx, npty, nptz
 
+
+import numpy as np
+
+def getIdx(value, val_min, val_max=None, npts=None, dval=None):
+    """
+    Return the index of a 1D value on a linear grid.
+    Either val_max and npts
+    or
+    dval
+    must be present
+    """
+    if (dval==None):
+        dval = (val_max-val_min)/(npts - 1)
+    return np.round((value-val_min)/dval)
+
+def getIdx_cube(x, y, z, xmin, xmax, nptx, ymin, ymax, npty, zmin, zmax, nptz):
+    """
+    Returns the index of a point on a 3D rectangular gridr
+    The cube order is assumed (from http://paulbourke.net/dataformats/cube/)
+       for (ix=0;ix<NX;ix++) {
+           for (iy=0;iy<NY;iy++) {
+               for (iz=0;iz<NZ;iz++) {
+                   printf("%g ",data[ix][iy][iz]);
+                   if (iz % 6 == 5)
+                       printf("\n");
+                   }
+               printf("\n");
+           }
+       }
+    """
+    idx = getIdx(x, xmin, xmax, nptx)
+    idy = getIdx(y, ymin, ymax, npty)
+    idz = getIdx(z, zmin, zmax, nptz)
+    index = idx * npty * nptz + idy * nptz + idz
+    return index
 
