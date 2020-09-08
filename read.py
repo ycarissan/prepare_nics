7af075d8506912e7346b5400fca12d5081d1f26d
alarru <albertartigas90@gmail.com>
2020-09-08 16:38:01 +0200

    small modification

--- read.py
+++ read.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 # Read input files with sets of coordinates and print one list for each set of coordinates
 
 file = open("coords.txt")
 
 for line in file:
-    list = []
     coords = line.split()
     print(coords)
-=======
-# This porgram open a file named "input.txt"
-#Each line contains atom numbers which define a site to add a ghost atom
->>>>>>> 1fdbb603ef458a1a9044a2e0b41498587eecb2bb
