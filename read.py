0fbd15d0d8d88f379750560642a048be6fea0a93
ycarissan <ycarissan@gmail.com>
2020-12-16 11:12:21 +0100

    cleaning up

--- read.py
+++
@@ -1,8 +0,0 @@
-
-# Read input files with sets of coordinates and print one list for each set of coordinates
-
-file = open("coords.txt")
-
-for line in file:
-    coords = line.split()
-    print(coords)
